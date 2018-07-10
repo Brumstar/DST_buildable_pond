d3367088783b10c07173bb060f382e59cc72e07f
Colin <cmacbrum@gmail.com>
2018-07-09 21:12:51 -0400

    Pond icon shows up properly

--- modmain.lua
+++ modmain.lua
@@ -4,32 +4,16 @@ local RECIPE = GLOBAL.Recipe
 local Ingredient = GLOBAL.Ingredient
 local TECH = GLOBAL.TECH
 
-
----return Prefab("pighouse", fn, assets, prefabs),
----    MakePlacer("pighouse_placer", "pig_house", "pig_house", "idle")
-
 Assets = 
 {
-	Asset("ATLAS", "images/inventoryimages/cellar.xml"),
- 	Asset("IMAGE", "images/inventoryimages/cellar.tex"),
-	Asset("IMAGE", "minimap/cellar.tex" ),
-        Asset("ATLAS", "minimap/cellar.xml" ),
+	Asset("ATLAS", "images/inventoryimages/buildable_pond.xml"),
+ 	Asset("IMAGE", "images/inventoryimages/buildable_pond.tex"),
 }
 
 PrefabFiles = { "buildable_pond" }
 
-AddMinimapAtlas("minimap/cellar.xml")
+AddMinimapAtlas("images/inventoryimages/buildable_pond.xml")
 
-STRINGS.RECIPE_DESC.POND = "Cold and wet"
---local buildable_pond = RECIPE("pond",{Ingredient("twigs", 1)}, RECIPETABS.FARM, TECH.NONE, "buildable_pond_placer")
---buildable_pond.atlas = "images/inventoryimages/cellar.xml"
+STRINGS.RECIPE_DESC.POND = "Cold and wet."
 
-AddRecipe("pond", {Ingredient("twigs", 1)}, RECIPETABS.FARM,  TECH.NONE, "buildable_pond_placer", nil, nil, nil, nil,  "images/inventoryimages/cellar.xml", "cellar.tex" )
-
-
-
-local function BuildablePondPostInit( inst )
-    return inst
-end
- 
-AddPrefabPostInit( "buildable_pond", BuildablePondPostInit)
+AddRecipe("pond", {Ingredient("twigs", 1)}, RECIPETABS.FARM,  TECH.NONE, "buildable_pond_placer", nil, nil, nil, nil,  "images/inventoryimages/buildable_pond.xml", "buildable_pond.tex" )
